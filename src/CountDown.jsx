e861d20b51634605d09613a7926e653c8a7a9165
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-11 23:13:32 +0530

    functionality added

--- src/CountDown.jsx
+++ src/CountDown.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 import { FaPlayCircle } from "react-icons/fa";
 import { FaCirclePause } from "react-icons/fa6";
@@ -9,10 +9,43 @@ const CountDown = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const hours = 0;
 
-  const handlePlay = () => {};
-  const handlePause = () => {};
-  const handleReset = () => {};
+  useEffect(() => {
+    let countdownInterval;
+
+    if (isActive) {
+      countdownInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        } else {
+          if (minutes > 0) {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          } else {
+            clearInterval(countdownInterval);
+            setIsActive(false);
+          }
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(countdownInterval);
+  }, [isActive, minutes, seconds]);
+
+  const handlePlay = () => {
+    if (!isActive) {
+      setIsActive(true);
+    }
+  };
+  const handleReset = () => {
+    setIsActive(false);
+    setMinutes(inputMinutes);
+    setSeconds(0);
+  };
+  const handlePause = () => {
+    setIsActive(false);
+  };
 
   const handleMinutesChange = (e) => {
     const inputValue = e.target.value;
@@ -37,11 +70,19 @@ const CountDown = () => {
           />
         </div>
 
-        <div className="buttons">
-          <FaPlayCircle onClick={handlePlay} />
-          <FaCirclePause onClick={handlePause} />
+        <span className="buttons">
+          <FaPlayCircle onClick={handlePlay} disabled={isActive} />
+          <FaCirclePause onClick={handlePause} disabled={!isActive} />
           <BiReset onClick={handleReset} />
-        </div>
+        </span>
+        <span>
+          <p className="display">
+            {hours >= 0 && `${hours.toString().padStart(2, "0")}:`}
+            {`${minutes.toString().padStart(2, "0")}:${seconds
+              .toString()
+              .padStart(2, "0")}`}
+          </p>
+        </span>
       </div>
     </div>
   );
